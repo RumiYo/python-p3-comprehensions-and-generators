af9e743d1f6e82c8bbfdd903a91015bd02189b00
RumiYo <yokoyamarumi@gmail.com>
2024-02-21 18:17:02 -0500

    Complete lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers = [n for n in num_list if n % 2 == 0 ]
+    return even_numbers
 
 def make_exclamation(sentence_list):
-    pass
+    add_exclamation = (n + "!" for n in sentence_list )
+    sentence_exclamation = list()
+    for m in add_exclamation:
+        sentence_exclamation.append(m)
+    return sentence_exclamation 
